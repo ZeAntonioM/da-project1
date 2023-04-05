1b7efc49679ba5f52e7736d6edf5da6436d19ba8
DiogoAlexandreOliveiraDaSilva <afkflex@gmail.com>
2023-04-05 20:42:14 +0100

    Done documentation for Displayers

--- src/services/actions/ShowStations.cpp
+++ src/services/actions/ShowStations.cpp
@@ -4,21 +4,10 @@
 
 #include "ShowStations.h"
 
-/**
- * @brief Constructor of the ShowStations, a class that extends Action
- * @param graph graph that stores all info
- */
-ShowStations::ShowStations(Graph &graph):Action(graph){};
+ShowStations::ShowStations(Graph &graph) : Action(graph){};
 
-/**
- * @brief Draws a table to display values, uses a system of pagination that displays 10 values per page
- * @tparam Station type of the value we want to display
- * @param data the values to display
- * @param page current page
- * @param nPages number of pages that exist
- * complexity O(1)
- */
-void ShowStations::draw(vector<Station> data ,int page, int npages){
+void ShowStations::draw(vector<Station> data, int page, int npages)
+{
     system("clear");
     cout << "\033[0m";
     cout << "┌────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐" << endl;
@@ -30,18 +19,18 @@ void ShowStations::draw(vector<Station> data ,int page, int npages){
     cout << "│\033[40m────────────────────────────────────────────────────────────────────────────────────────────────────────────────\033[0m│" << endl;
     cout << "│\033[40m Name                                 │ Municipality               │ District        │ Line                     \033[0m│" << endl;
     cout << "│\033[40m────────────────────────────────────────────────────────────────────────────────────────────────────────────────\033[0m│" << endl;
-    for (int i = 10 * page; i < 10 * page + 10; i++) {
+    for (int i = 10 * page; i < 10 * page + 10; i++)
+    {
         if (i == data.size())
             break;
-        cout<<"│";
+        cout << "│";
         if (i % 2 == 0)
             cout << "\033[47m "
                  << "\033[30m";
         else
             cout << "\033[100m ";
         data[i].print();
-        cout<<"\033[0m│"<<endl;
-
+        cout << "\033[0m│" << endl;
     }
 
     cout << "│\033[40m────────────────────────────────────────────────────────────────────────────────────────────────────────────────\033[0m│" << endl;
@@ -49,80 +38,77 @@ void ShowStations::draw(vector<Station> data ,int page, int npages){
     cout << "|\033[40m────────────────────────────────────────────────────────────────────────────────────────────────────────────────\033[0m|" << endl;
 };
 
-/**
- * @brief Displays all the Stations
- * @see Show::paginationController(vector<Station> data)
- * Complexity: O(N log(N)) being N the number of airports
- */
-void ShowStations::execute() {
+void ShowStations::execute()
+{
     vector<Station> stations;
-    for (auto station : graph->getStationSet()) {
+    for (auto station : graph->getStationSet())
+    {
         stations.push_back(*station);
     }
     paginationController(stations);
     wait();
 }
 
-/** Controls the pagination of the drawn table. It allows the user to quit the menu, or jump to the next, previous or any other page directly.
- * @brief Controls the pagination of the drawn table.
- * @see show::draw(vector<Name> data, int page, int nPages)
- * @tparam Station type of the values we want to display
- * @param data the values we want to display
- * complexity O(1)
- */
-void ShowStations::paginationController(vector<Station> data) {
+void ShowStations::paginationController(vector<Station> data)
+{
     /* due to the template this functions had to be written here as if it was on the cpp it would not recognize the template*/
     int page = 0;
     while (page >= 0 and page < (float)data.size() / 10.0)
     {
         string option;
-        draw(data,page,ceil((float)data.size()/10.0));
+        draw(data, page, ceil((float)data.size() / 10.0));
         bool cond = true;
         while (cond)
         {
             cout << endl
-                 << "\033[32mChoose an option[n/p/q] or the number of the page you would want to go[1-"<<ceil((float)data.size()/10.0)<<"]: ";
+                 << "\033[32mChoose an option[n/p/q] or the number of the page you would want to go[1-" << ceil((float)data.size() / 10.0) << "]: ";
             cond = true;
             cin >> option;
 
             if (option.length() == 1)
             {
-                option= ::toupper(option[0]);
+                option = ::toupper(option[0]);
                 switch (option[0])
                 {
-                    case 'N':
-                        page++;
-                        cond=false;
-                        break;
-                    case 'P':
-                        page--;
-                        cond=false;
-                        break;
-                    case 'Q':
-                        page = -1;
-                        cond=false;
-                        break;
-                    default:
-                        cond = true;
+                case 'N':
+                    page++;
+                    cond = false;
+                    break;
+                case 'P':
+                    page--;
+                    cond = false;
+                    break;
+                case 'Q':
+                    page = -1;
+                    cond = false;
+                    break;
+                default:
+                    cond = true;
                 }
             }
-            if(cond){
+            if (cond)
+            {
                 int test;
-                try{
-                    cond=false;
-                    test= stoi(option);
-                }catch (invalid_argument){
-                    cond=true;
+                try
+                {
+                    cond = false;
+                    test = stoi(option);
                 }
-                if(!cond){
-                    cond=true;
-                    if(to_string(test).length()==option.length()){
-                        if(test>0 and test <=ceil((float)data.size()/10.0)) {
-                            page=test-1;
-                            cond= false;
+                catch (invalid_argument)
+                {
+                    cond = true;
+                }
+                if (!cond)
+                {
+                    cond = true;
+                    if (to_string(test).length() == option.length())
+                    {
+                        if (test > 0 and test <= ceil((float)data.size() / 10.0))
+                        {
+                            page = test - 1;
+                            cond = false;
                         }
                     }
-
                 }
             }
             if (cond)
@@ -132,4 +118,3 @@ void ShowStations::paginationController(vector<Station> data) {
         }
     }
 };
-
--- src/services/actions/ShowStations.h
+++ src/services/actions/ShowStations.h
@@ -8,11 +8,36 @@
 #include "Action.h"
 #include "math.h"
 
-class ShowStations:public Action{
+class ShowStations : public Action
+{
 public:
-    void draw(vector<Station> data ,int page, int npages);
+    /**
+     * @brief Constructor of the ShowStations, a class that extends Action
+     * @param graph graph that stores all info
+     */
+    void draw(vector<Station> data, int page, int npages);
+    /**
+     * @brief Draws a table to display values, uses a system of pagination that displays 10 values per page
+     * @tparam Station type of the value we want to display
+     * @param data the values to display
+     * @param page current page
+     * @param nPages number of pages that exist
+     * complexity O(1)
+     */
     ShowStations(Graph &graph);
+    /**
+     * @brief Displays all the Stations
+     * @see Show::paginationController(vector<Station> data)
+     * Complexity: O(N log(N)) being N the number of stations
+     */
     void execute() override;
+    /** Controls the pagination of the drawn table. It allows the user to quit the menu, or jump to the next, previous or any other page directly.
+     * @brief Controls the pagination of the drawn table.
+     * @see show::draw(vector<Name> data, int page, int nPages)
+     * @tparam Station type of the values we want to display
+     * @param data the values we want to display
+     * complexity O(1)
+     */
     void paginationController(vector<Station> data);
 };
 #endif
